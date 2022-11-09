839edae1886832a7cec9000deb5970056d2e16c6
LDY <leedy3838@naver.com>
2022-11-09 19:39:17 +0900

    과목 선택에서 전체 문제 수 출력

--- app/src/main/java/com/gongbok/SubjectSelectScreen.java
+++ app/src/main/java/com/gongbok/SubjectSelectScreen.java
@@ -1,18 +1,14 @@
 package com.gongbok;
 
-import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
+import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -24,16 +20,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class SubjectSelectScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subject_select_screen);
 
         List<SubjectData> DataList = new LinkedList<>();
-
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         DocumentReference docRef = db.collection("cities").document("SF");
 
         db.collection("문제")
@@ -50,11 +43,20 @@ public class SubjectSelectScreen extends AppCompatActivity {
                                 DataList.add(data);
                             }
 
+                            //RecyclerView에 목록 출력
                             RecyclerView recyclerView = findViewById(R.id.subjectList) ;
                             recyclerView.setLayoutManager(new LinearLayoutManager(SubjectSelectScreen.this));
 
                             SubjectAdapter adapter = new SubjectAdapter(DataList);
                             recyclerView.setAdapter(adapter);
+
+                            //전체 문제 수 출력
+                            TextView totalProblemNum = findViewById(R.id.totalProblemNum);
+                            int problemSum = 0;
+                            for(int i = 0; i<DataList.size(); i++)
+                                problemSum += DataList.get(i).count;
+                            //setText 안에 int 쓰면 안되더라구요...
+                            totalProblemNum.setText(String.valueOf(problemSum));
                         }
                     }
                 });
