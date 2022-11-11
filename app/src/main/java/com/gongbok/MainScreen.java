a25df33e85dda2fa193ac5437f7cfcf2ce23cb8c
kyowon <kskw20@naver.com>
2022-11-11 13:51:15 +0900

    로그인 & 회원가입 구현 완료 (수정 사항 발생 시 추가 커밋 예정)

--- app/src/main/java/com/gongbok/MainScreen.java
+++ app/src/main/java/com/gongbok/MainScreen.java
@@ -2,17 +2,37 @@ package com.gongbok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainScreen extends AppCompatActivity {
 
+    private GoogleSignInAccount gsa;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
+
+    public void logOut(View v) {
+        // 이메일/패스워드 사용자 로그아웃
+        firebaseAuth.signOut();
+        // 구글 사용자 로그아웃
+        setResult(RESULT_OK);
+
+        finish();
+    }
+
 
     public void goToMyProblem(View view) {
         startActivity(new Intent(this, MyProblem.class));
