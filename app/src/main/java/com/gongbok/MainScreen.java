b6df850b1045695f4c787c4520a6662b77f154cf
iizuna <iizuna@naver.com>
2022-11-13 19:30:25 +0900

    이름 변경 Rank_Icon ex) rankIcons_s1 -> rank_icons_s1
    파이어베이스 적용
    ex) 레이팅, 유저 이름 받아와서 적용

--- app/src/main/java/com/gongbok/MainScreen.java
+++ app/src/main/java/com/gongbok/MainScreen.java
@@ -1,19 +1,90 @@
 package com.gongbok;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.SortedList;
+import androidx.recyclerview.widget.SortedListAdapterCallback;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainScreen extends AppCompatActivity {
+
+    //Firebase
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
-    }
 
+        //Firebase 세팅
+        db = FirebaseFirestore.getInstance();
+
+        //임시 userID
+        String userID = "test";
+        db.collection("유저")
+                .document(userID)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()){
+                            //화면에 레이팅 반영
+                            Map<String, Object> fieldValues = task.getResult().getData();
+                            Long rate = (Long)fieldValues.get("레이팅");
+                            TextView ratingValue = (TextView)findViewById(R.id.ratingValue);
+                            ratingValue.setText("Rating " + rate);
+
+                            //화면에 닉네임 반영
+                            TextView nickName = (TextView)findViewById(R.id.nickName);
+                            nickName.setText(userID);
+                        }
+                    }
+                });
+        db.collection("유저")
+                .document(userID)
+                .collection("푼 문제")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            List<DocumentSnapshot> fieldValues = task.getResult().getDocuments();
+                            long problemRates[] = new long[fieldValues.size()];
+                            for (int i=0;i<fieldValues.size();i++){
+
+                            }
+                        }
+                    }
+                });
+    }
     public void goToMyProblem(View view) {
         startActivity(new Intent(this, MyProblem.class));
     }
