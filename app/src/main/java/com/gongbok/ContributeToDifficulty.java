8a31d364444615bd18bcd38089ea4c50d1694b5e
LDY <leedy3838@naver.com>
2022-11-20 21:56:04 +0900

    난이도 평가 화면 구현 및 하단 바 제거

---
+++ app/src/main/java/com/gongbok/ContributeToDifficulty.java
@@ -0,0 +1,26 @@
+package com.gongbok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ContributeToDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contribute_to_difficulty);
+    }
+
+    public void goToMain(View view) {
+        startActivity(new Intent(this, MainScreen.class));
+    }
+    public void goToProblemSolve(View view) {
+        startActivity(new Intent(this, ProblemSolveScreen.class));
+    }
+    public void goToEnrollProblem(View view) {
+        startActivity(new Intent(this, EnrollProblemScreen.class));
+    }
+}
